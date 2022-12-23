abf7b40869f634a659451091f31e9ca9d1a4611b
TranTheDuy124 <trantheduy06203@gmail.com>
2022-12-23 09:30:17 +0700

    helloworld

---
+++ main.js
@@ -0,0 +1,8 @@
+$('.BL').click(function(){
+    $('body').animate({scrollTop:$('#BL').offset().top});
+    return false;
+})
+$('.BC').click(function(){
+    $('body').animate({scrollTop:$('#BC').offset().top});
+    return false;
+})
