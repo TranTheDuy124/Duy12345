0435e6870f171fb16c2f310cd9f1ae971fe6bf45
TranTheDuy124 <102197522+TranTheDuy124@users.noreply.github.com>
2023-03-10 16:33:38 +0700

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-$('.BL').click(function(){
-    $('body').animate({scrollTop:$('#BL').offset().top});
-    return false;
-})
-$('.BC').click(function(){
-    $('body').animate({scrollTop:$('#BC').offset().top});
-    return false;
-})
